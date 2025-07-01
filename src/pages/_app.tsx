5b71c6d6b5164616e92aaa83af1303fdbfdccace
DANSRM02 <danielmercenaries417@gmail.com>
2025-07-01 07:49:25 -0500

    feat(Page Routing) Refactoring prev. solution adding unique layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/layout/footer";
 import MainHeader from "@/components/layout/header";
 import type { AppProps } from "next/app";
-import "../globals.css";
+import "./globals.css";
 
 function MainLayout({ Component, pageProps }: AppProps) {
   return (
