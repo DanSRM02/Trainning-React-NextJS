c5218447265aa2a0ce86a6635a42fc25b404dcd6
DANSRM02 <danielmercenaries417@gmail.com>
2025-07-09 16:42:32 -0500

    feat(makeup) Adding guide to the contribute

---
+++ src/app/blog/password-checker/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// ERICK ENCARGUESE UNICAMENTE DEL HTML QUE SEA SEMANTICO Y CON ARIA 
+// NADA DE FUNCIONALIDAD YO ME ENCARGO DE ESO (DANIEL) 
+
+/* ELIMINAR ESTE COMENTARIO CUANDO TERMINE Y AVISAR*/
+
+function PasswordChecker() {
+  return (
+    <div>PasswordChecker</div>
+  )
+}
+
+
+
+export default PasswordChecker
