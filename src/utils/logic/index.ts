51d2349eb9b8ea1d4376c5a38521f1583bb58551
DANSRM02 <danielmercenaries417@gmail.com>
2025-07-10 18:14:24 -0500

    feat(page): Add checker password functionality on the page

---
+++ src/utils/logic/index.ts
@@ -0,0 +1,44 @@
+import { SecurityLevel } from "@/components/Card/types/CardTypes";
+
+export function getSecurityLevelFromScore(score: number): SecurityLevel {
+  if (score >= SecurityLevel.high) return SecurityLevel.high;
+  if (score >= SecurityLevel.medium) return SecurityLevel.medium;
+  if (score >= SecurityLevel.low) SecurityLevel.low;
+  if (score > 0) return SecurityLevel.none;
+  return SecurityLevel.none;
+}
+
+export function calculatePasswordStrength(password: string): number {
+  let score = 0;
+
+  const uppercaseRegExp = /[A-Z]/;
+  const lowercaseRegExp = /[a-z]/;
+  const numbersRegExp = /[0-9]/;
+  const specialCharsRegExp = /[^A-Za-z0-9]/;
+
+  if (password.length >= 8) score += 10;
+  if (password.length >= 12) score += 15;
+  if (password.length >= 16) score += 15;
+
+  const hasUppercase = uppercaseRegExp.test(password);
+  const hasLowercase = lowercaseRegExp.test(password);
+  const hasNumbers = numbersRegExp.test(password);
+  const hasSpecialChars = specialCharsRegExp.test(password);
+
+  let typesCount = 0;
+  if (hasUppercase) typesCount++;
+  if (hasLowercase) typesCount++;
+  if (hasNumbers) typesCount++;
+  if (hasSpecialChars) typesCount++;
+
+  score += typesCount * 10;
+
+  if (typesCount >= 3) {
+    score += 15;
+  }
+  if (typesCount === 4) {
+    score += 15;
+  }
+
+  return Math.min(score, 100);
+}
