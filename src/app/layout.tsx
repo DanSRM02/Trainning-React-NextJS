789866034f69fd155ef0641fc1c0064b87ec9245
DANSRM02 <danielmercenaries417@gmail.com>
2025-07-14 17:32:50 -0500

    hotFix(Styles): SOlution for sticky footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>        
         <div className="main-layout">
           <Header />
-          <main>{children}</main>
+          <main className="main-content">{children}</main>
           <Footer />
         </div>
       </body>
